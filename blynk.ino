76bf2f6fa5a68b3d641c49ba2e0a8e9dcd4e99a9
Nidhi Kumari <nidhi1691996@outlook.com>
2018-06-22 14:03:05 +0530

    Add files via upload

---
+++ blynk.ino
@@ -0,0 +1,25 @@
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "efa45a37f530478baf97558cbc90b58d";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "abhijit";
+char pass[] = "abhijit130";
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+blynk.txt
+Displaying blynk.txt.
